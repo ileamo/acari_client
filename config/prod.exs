b2b5f99bcb8bdd8bcfc10aad4bafec7f34c6a519
Igor Mosunov <imosunov@nsg.ru>
2019-01-28 16:01:00 +0300

    Файлы для подготовки релиза

---
+++ config/prod.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, format: "[$level] $message\n", level: :debug
+
+
+config :acari_client, port: 7000
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule AcariClient.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :ssl],
       mod: {AcariClient.Application, []}
     ]
   end
